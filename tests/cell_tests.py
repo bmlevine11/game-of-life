3d226e5c61a51aa4859837682443a5bbc542ad0b
Brian Levine <bmlevine11@gmail.com>
2014-05-07 03:00:36 -0400

    minor test changes

--- tests/cell_tests.py
+++ tests/cell_tests.py
@@ -6,6 +6,8 @@ def test_does_cell_exist():
 	"Does the cell object exist and instantiate properly?"
 	testCell = cell(1,2,True)
 	assert_equal(testCell.x, 1)
+	assert_equal(testCell.y, 2)
+	assert_equal(testCell.alive, True)
 
 def test_cell_die():
 	"Can the cell die?"
@@ -17,4 +19,9 @@ def test_cell_respawn():
 	"Can the cell rise from the dead?"
 	testCell = cell(1,2,True)
 	testCell.respawn
+	assert_equal(testCell.alive, True)
+
+def test_is_alive():
+	"Is the cell dead or alive?"
+	testCell = cell(1,2,True)
 	assert_equal(testCell.alive, True)
